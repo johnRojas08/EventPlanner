f5930007ed3a49bc0f0b49c421a2e0a4a9b3f6c4
johnRojas08 <johnrojas0808@gmail.com>
2024-12-14 09:25:33 +0100

    Añadiendo Priority enum para definir los nieveles de priority event

---
+++ src/Priority.java
@@ -0,0 +1,6 @@
+// Enum para definir los niveles de prioridad de los eventos
+public enum Priority {
+    HIGH,    // Alta prioridad
+    MEDIUM,  // Prioridad media
+    LOW      // Baja prioridad
+}
